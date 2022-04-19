d92408927b57d3641c0d38c113eb83de8d9fd0d3
João Victor Costa Santos <rm88372@fiap.com.br>
2022-04-19 19:54:02 -0300

    Creating Client Entity and Creating H2 DataBase.

--- backend/src/main/java/com/vcosmus/crudclients/entities/Client.java
+++ backend/src/main/java/com/vcosmus/crudclients/entities/Client.java
@@ -1,4 +1,31 @@
 package com.vcosmus.crudclients.entities;
 
-public class Client {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.Instant;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "tb_client")
+public class Client implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String cpf;
+    private Double income;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant birthDate;
+    private Integer children;
 }
